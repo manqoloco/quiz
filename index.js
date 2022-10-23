49e27e17b1854a0dfa72208c851e90b462f9cbab
olivia <oliviajmarongiu@gmail.com>
2022-10-23 18:54:21 +0200

    changed color for result

--- index.js
+++ index.js
@@ -1,102 +1,98 @@
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
 let myArray = [
-    {
-       question: "do pigs fly?",
-       answer: false
-    },
-    {
-        question: "do cows really exist?",
-        answer: true 
-    },
-    {
-        question: "is Halloween in September?",
-        answer: false
-    },
-    {
-        question: "does 2+2 = 4?",
-        answer: true
-    },
-    {
-        question: "is Gothenburg the capital of Sweden?",
-        answer: false
-    },
-    {
-        question: "is Sweden's flag yellow and blue?",
-        answer: true
-    },
-    {
-        question: "is norwegian the official language in Sweden?",
-        answer: false
-    },
-    {
-        question: "do cats usually have 6 legs?",
-        answer: false
-    },
-    {
-        question: "do carnivores eat meat?",
-        answer: true
-    },
+  {
+    question: "do pigs fly?",
+    answer: false,
+  },
+  {
+    question: "do cows really exist?",
+    answer: true,
+  },
+  {
+    question: "is Halloween in September?",
+    answer: false,
+  },
+  {
+    question: "does 2+2 = 4?",
+    answer: true,
+  },
+  {
+    question: "is Gothenburg the capital of Sweden?",
+    answer: false,
+  },
+  {
+    question: "is Sweden's flag yellow and blue?",
+    answer: true,
+  },
+  {
+    question: "is norwegian the official language in Sweden?",
+    answer: false,
+  },
+  {
+    question: "do cats usually have 6 legs?",
+    answer: false,
+  },
+  {
+    question: "do carnivores eat meat?",
+    answer: true,
+  },
 
-    {
-        question: "do cats have 9 lives?",
-        answer: false
-    }
-
-]
+  {
+    question: "do cats have 9 lives?",
+    answer: false,
+  },
+];
 
 let showResBtn = document.querySelector("#showResults");
 
 showResBtn.addEventListener("click", () => {
   let result = 0;
-    for (let i = 1; i <= myArray.length; i++) {
-    let answer = document.querySelector(
-        `[name='answer${i}']:checked`
-    );
-    if(!answer) {
-        console.log("try again");
-        return;
+  for (let i = 0; i < myArray.length; i++) {
+    let answer = document.querySelector(`[name='input${i}']:checked`);
+    if (!answer) {
+      console.log("try again");
+      return;
     }
-    if (myArray[i-1].answer == (answer === 'true')){
-       result++;
+    let a = answer.value === "true";
+    console.log(a);
+    if (myArray[i].answer == a) {
+      result++;
     }
   }
 
   console.log(result);
   let x = document.querySelector("#result");
   x.value = result;
-  
+
+  let p = (result / myArray.length) * 100;
+
+
+  if (p < 50 ) {
+    x.style.color = "red";
+  } else if (p >= 50 && p <= 75) {
+    x.style.color = "orange";
+  } else {
+    x.style.color = "green";
+  }
 });
 
+let index = 0;
+let result = myArray
+  .map((x) => {
+    return `
+        <fieldset> 
+        <legend>${x.question}</legend>
+        <label>true</label>
+        <input value="true" name="input${index}" type="radio"/>
+        <label>false</label>
+        <input value="false" name="input${index++}" type="radio"/>
 
-
-
+        </fieldset>`;
+  })
+  .join("");
 let questions = document.querySelector("#questions");
-
-let i = 1;
-myArray.forEach(q => {
-   let div = document.createElement("div");
-   div.appendChild(document.createElement("h1").appendChild(document.createTextNode(q.question)));
-   let label1 = document.createElement("label");
-   label1.for = `answer${i}A`;
-   label1.innerHTML = "true";
-   div.appendChild(label1);
-   let input1 = document.createElement("input");
-   input1.type = "radio";
-   input1.name = `answer${i}`;
-   input1.id = `answer${i}A`;
-   input1.value = "true";
-   div.appendChild(input1);
-   let label2 = document.createElement("label");
-   label2.for = `answer${i}B`;
-   label2.innerHTML = "false";
-   div.appendChild(label2);
-   let input2 = document.createElement("input");
-   input2.type = "radio";
-   input2.name = `answer${i}`;
-   input2.id = `answer${i}B`;
-   input2.value = "false";
-   div.appendChild(input2);   
-   questions.appendChild(div);
-    i++;
-
-});
+questions.innerHTML = result;
