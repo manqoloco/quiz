edb8b146e510e0a1e80fdbd70bba73cdfd23e842
olivia <oliviajmarongiu@gmail.com>
2022-10-24 14:10:21 +0200

    tried to add checkboxes(no answer options)

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 function myFunction() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-}
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+  
+  
+  const QueryType = {
+  TrueFalse: 0,
+  Multiple: 1,
+  Checkboxes: 2,
+};
+
+
 
 let myArray = [
   {
     question: "do pigs fly?",
     answer: false,
+    type: QueryType.TrueFalse,
   },
   {
+    question: "when is my birthday?",
+    choices: ["may 6th", "june 6th", "june 16th", "june 26th"],
+    answer: "june 16th",
+    type: QueryType.Multiple,
+  },
+  {
+    question: "which ones are cities in Sweden?",
+    choices: ["Gothenburg", "London", "Stockholm", "Rome"],
+    answers: ["Gothenburg", "Stockholm"],
+    type: QueryType.Checkboxes,
+  },
+  /* {
     question: "do cows really exist?",
     answer: true,
   },
@@ -44,7 +66,7 @@ let myArray = [
   {
     question: "do cats have 9 lives?",
     answer: false,
-  },
+  }, */
 ];
 
 let showResBtn = document.querySelector("#showResults");
@@ -70,8 +92,7 @@ showResBtn.addEventListener("click", () => {
 
   let p = (result / myArray.length) * 100;
 
-
-  if (p < 50 ) {
+  if (p < 50) {
     x.style.color = "red";
   } else if (p >= 50 && p <= 75) {
     x.style.color = "orange";
@@ -83,7 +104,8 @@ showResBtn.addEventListener("click", () => {
 let index = 0;
 let result = myArray
   .map((x) => {
-    return `
+    if (x.type == QueryType.TrueFalse) {
+      return `
         <fieldset> 
         <legend>${x.question}</legend>
         <label>true</label>
@@ -92,6 +114,34 @@ let result = myArray
         <input value="false" name="input${index++}" type="radio"/>
 
         </fieldset>`;
+    } else if (x.type == QueryType.Multiple) {
+      let options = x.choices
+        .map((c) => {
+          return `<option value="${c}">${c}</option>`;
+        })
+        .join("");
+      return `
+          <fieldset> 
+          <legend>${x.question}</legend>
+          <select id="${index}">
+          ${options}
+        </select>
+
+
+  
+          </fieldset>`;
+    } else if (x.type == QueryType.Checkboxes) {
+      let boxes = x.choices
+        .map((b) => {
+          return `<input type="checkbox" id="${index}"></input> `;
+        })
+        .join("");
+      return ` 
+       <fieldset>
+       <legend>${x.question}</legend>
+       ${boxes}
+       </fieldset>`;
+    }
   })
   .join("");
 let questions = document.querySelector("#questions");
